433364431044e062eb1d20ae46a5f32f1ebf6192
garciasdiogo <dggarcias00@gmail.com>
2024-04-08 16:14:38 -0300

    Estrutuas de controle 2

---
+++ estruturas de controle 2/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Estrutura de controle: if
+	idade := 18
+	if idade >= 18 {
+		fmt.Println("Você é maior de idade.")
+	} else {
+		fmt.Println("Você é menor de idade.")
+	}
+
+	//Estrutura de controle: for
+	for i := 0; i < 5; i++ {
+		fmt.Println("Número:", i)
+	}
+
+	//Estrutura de controle: switch
+	dia := "sábado"
+	switch dia {
+	case "segunda", "terça", "quarta", "quinta", "sexta":
+		fmt.Println("Dia útil")
+	case "sábado", "domingo":
+		fmt.Println("Fim de semana")
+	default:
+		fmt.Println("Dia inválido")
+	}
+}
