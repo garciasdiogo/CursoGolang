38f9d3994248a5da02155ba3adaecba19cab1cca
garciasdiogo <dggarcias00@gmail.com>
2024-04-08 17:44:12 -0300

    Atualização

--- estruturas de controle 2/main.go
+++ estruturas de controle 2/main.go
@@ -28,29 +28,6 @@ func main() {
 		fmt.Println("Dia inválido")
 	}
 
-	// Estrutura de controle: if
-	idade = 18
-	if idade >= 18 {
-		fmt.Println("Você é maior de idade.")
-	} else {
-		fmt.Println("Você é menor de idade.")
-	}
-
-	//Estrutura de controle: for
-	for i := 0; i < 5; i++ {
-		fmt.Println("Número:", i)
-	}
-
-	//Estrutura de controle: switch
-	dia = "sábado"
-	switch dia {
-	case "segunda", "terça", "quarta", "quinta", "sexta":
-		fmt.Println("Dia útil")
-	case "sábado", "domingo":
-		fmt.Println("Fim de semana")
-	default:
-		fmt.Println("Dia inválido")
-	}
 	/*
 		if é usado para fazer verificações condicionais.
 		for é usado para loops.
